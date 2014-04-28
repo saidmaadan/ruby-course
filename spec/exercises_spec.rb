057204a3c089ecf6ba86646d6de8254c62b3ce69
Said Maadan <saidmaadan@gmail.com>
2014-04-28 14:45:36 -0500

    Add code to pass ex1 method

---
+++ spec/exercises_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../exercises.rb'
+
+
+describe 'Exercise' do
+  it "triples the length of a string" do
+    result = Exercises.ex0("ha")
+    expect(result).to eq("hahaha")
+  end
+
+  it "returns 'nope' if the string is 'wishes'" do
+    result = Exercises.ex0("wishes")
+    expect(result).to eq("nope")
+  end
+
+  it reyurn the second element of an array do
+  end
+end
+
+
+
